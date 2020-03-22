559f5d6336e74386d4054bac331ddefa399d5883
eogodo <eogodo@ikigaicompanies.com>
2020-03-22 12:17:02 -0400

    update

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,15 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+tableData.forEach(function(sighting) {
+  console.log(sighting);
+  var row = tbody.append("tr");
+
+  Object.entries(sighting).forEach(function([key, value]) {
+    console.log(key, value);
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
